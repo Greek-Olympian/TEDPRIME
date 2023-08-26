a1e11b30f6977c28e458dc0834842b6e81738e0a
Greek-Olympian <greekolympian0@gmail.com>
2023-08-25 16:04:38 -0800

    tee

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Web Designer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Web Developer", "Web Designer", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Web Designer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Web Developer", "Web Designer", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
